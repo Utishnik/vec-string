6442c48e11d941e01791f54e08ae23dd44f6f040
Utishnik <yrik.kokoyto.00@bk.ru>
2026-02-14 23:45:01 +0300

    no std + rewrite logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,42 @@
+#![no_std]
+
+extern crate alloc;
+
+use alloc::string::String;
+use alloc::vec::Vec;
+use alloc::format;
+
+pub type FormatRuleFn = fn(&str,usize,usize) -> String;
+
+pub const DEFAULT_FORMAT_RULE: FormatRuleFn = {
+    fn format_def(val: &str, index: usize,len: usize) -> String {
+        if index == 0 {
+            format!("[{}", val)
+        } 
+        else if index != len - 1{
+            format!(", {}", val)
+        }
+        else {
+            format!(", {}]", val)
+        }
+    }
+    format_def
+};
+
 pub trait VecString{
-    fn vec_string(&self) -> String;
-    fn vec_string_inner(&self) -> String;
+    fn vec_string(&self,format_rule: FormatRuleFn) -> String;
 }
 
 /// Get string of Vec<T> where T: Display
-impl<T> VecString for Vec<T> where T: std::fmt::Display{
-    /// assert_eq!("[1, 2, 3]", vec![1, 2, 3].vec_string());
-    fn vec_string(&self) -> String{
-        format!("[{}]", self.vec_string_inner())
-    }
-
+impl<T> VecString for Vec<T>
+where T: core::fmt::Display
+ {
     /// assert_eq!("1, 2, 3", vec![1, 2, 3].vec_string());
-    fn vec_string_inner(&self) -> String{
-        let mut string = String::new();
-        for x in self{
-            string.push_str(&format!("{}, ", x));
+    fn vec_string(&self,format_rule: FormatRuleFn) -> String{
+        let mut string: String = String::new();
+        for x in self.iter().enumerate(){
+            string.push_str(&format_rule(&format!("{}",x.1),x.0,self.len()));
         }
-        string.pop();
-        string.pop();
         string
     }
 }
@@ -25,6 +44,8 @@ impl<T> VecString for Vec<T> where T: std::fmt::Display{
 #[cfg(test)]
 mod tests {
     use crate::VecString;
+    use crate::DEFAULT_FORMAT_RULE;
+    use alloc::vec;
 
     #[test]
     fn it_works() {
@@ -33,7 +54,6 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!("[1, 2, 3]", vec![1, 2, 3].vec_string());
-        assert_eq!("1, 2, 3", vec![1, 2, 3].vec_string_inner());
+        assert_eq!("[1, 2, 3]", vec![1, 2, 3].vec_string(DEFAULT_FORMAT_RULE));
     }
 }
